f6bc3888ae7a9399dc4864cb10b0965f0bdb64da
wryyyy123 <78397101+wryyyy123@users.noreply.github.com>
2026-04-23 18:36:13 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: "./",
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
